fa30fb5fa6b322226e0fd56adab5ed2948f2d14c
mohamedalfahim <malfahim8@gmail.com>
2018-11-29 22:11:49 -0800

    (work-in-progress) fixed mistake

--- coolcalc/tools/sigfigs.py
+++ coolcalc/tools/sigfigs.py
@@ -14,16 +14,14 @@ def significant_figure_info(value: str):
         value, exponent = check_result
     if Decimal(value) == 0:
         return []  # Note that 0, 0.0, 0.00, and etc. have no sig-figs.
-    point_position = get_decimal_position(value)
     value = value.lstrip('0')
     i = 0
-    if point_position is None:  # No decimal point
-        for character in value:
-            # result.append((digit, position, rule))
-            if character == '0':
-                pass
-            elif character == '.':
-                pass
-            else:
-                result.append((character, i, 1))
-            i += 1
+    for character in value:
+        # result.append((digit, position, rule))
+        if character == '0':
+            pass
+        elif character == '.':
+            pass
+        else:
+            result.append((character, i, 1))
+        i += 1
